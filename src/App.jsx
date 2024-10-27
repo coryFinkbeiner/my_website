3ac38af3167b27dd37a1757e736ff5c87fa9e653
steeper <portland.steeper@gmail.com>
2024-10-26 20:46:22 -0400

    did i initialize?

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXhz9pJ-NlWlIrFeJdQe_gAq44Lsu7lJc",
+  authDomain: "website999-71344.firebaseapp.com",
+  projectId: "website999",
+  storageBucket: "website999.appspot.com",
+  messagingSenderId: "792979793137",
+  appId: "1:792979793137:web:aa71583e42bfa476a03550"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+console.log({app})
+
+
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
